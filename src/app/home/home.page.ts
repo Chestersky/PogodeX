28d8dfd207046d7aa8aa473e7c1dcfbfa2615689
Tomasz Szkaradek <tomasz.szkrdk@gmail.com>
2019-05-23 18:31:11 +0200

    Create basic UI

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ModalController } from "@ionic/angular";
+import { ModalComponent } from "app/modal/modal.component";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss']
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
-export class HomePage {}
+export class HomePage {
+  constructor(private modal: ModalController) {}
+
+  async openModal() {
+    console.log("here");
+    const modal = await this.modal.create({
+      component: ModalComponent,
+      componentProps: { value: 123 }
+    });
+    return await modal.present();
+  }
+}
