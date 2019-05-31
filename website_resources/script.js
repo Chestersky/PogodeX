79c387d61b31fb790579d1792ed358c64f39042a
Wojtek <yodeusz@gmail.com>
2019-05-31 21:57:56 +0200

    some cosmetic changes and modal with use case diagram added

--- website_resources/script.js
+++ website_resources/script.js
@@ -1,4 +1,4 @@
-//change background of the navbar 
+//change mavbar background
     window.addEventListener('scroll', function(e){
         var scrollPosition = window.pageYOffset;
         if (scrollPosition < 300) {
@@ -9,6 +9,30 @@
         }
         document.getElementById("menu").className = "topnav";
     })
+
+//open modal
+    function openModal(){
+        document.getElementById('modal').style.display = 'block';
+        document.getElementsByTagName('body')[0].style.overflow = 'hidden';
+    }
+
+//close modal
+    function closeModal(){
+        document.getElementById('modal').style.display = "none";
+        document.getElementsByTagName('body')[0].style.overflow = 'auto';
+    }
+
+//close the modal when click outside the image
+    var modal = document.getElementById('modal');
+    var modalWrapper = document.getElementsByClassName('modal-wrapper')[0];
+    window.onclick = function(event) {
+        if (event.target == modal || event.target == modalWrapper) {
+            modal.style.display = "none";
+            document.getElementById('modal-image').src = "";
+            document.getElementsByTagName('body')[0].style.overflow = 'auto';
+        }
+    }
+
 //scroll to section
     $(document).ready(function(){
         $('a[href^="#"]').on('click', function(event) {
