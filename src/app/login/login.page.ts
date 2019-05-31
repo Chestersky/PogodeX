fb5a8bed05355110b91e682944e3ab0737632322
Albert Stapor <askooler1@gmail.com>
2019-05-31 19:35:32 +0200

    login form connected with auth service

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { AuthService } from 'app/services';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
+
+  profileForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
 
   ngOnInit() {
+  }
+  
+  onSubmit() {
+    this.authService.signIn(this.profileForm.value.email, this.profileForm.value.password);
   }
 
 }
---
+++ src/config/weather.ts
@@ -0,0 +1,4 @@
+// Create new file as weather-api.ts and fill gaps
+export const weatherApi = {
+  key: '181a7ea078ff87918151d4baff6bfd6f'
+};
